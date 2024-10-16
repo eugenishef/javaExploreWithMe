5bc9cda16d0b615acc79f84da72f9aff7dc2a9ec
eugenishef <eugenishef@yandex.ru>
2024-10-16 23:47:15 +0300

    проверим как проходят тесты

--- stats-http-service/src/main/java/ru/practicum/stats/client/dto/StatisticsResponse.java
+++ stats-http-service/src/main/java/ru/practicum/stats/client/dto/StatisticsResponse.java
@@ -1,10 +1,12 @@
 package ru.practicum.stats.client.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class StatisticsResponse {
     private String app;
     private String uri;
