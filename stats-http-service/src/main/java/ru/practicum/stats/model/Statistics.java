b49ea6c6d8cc195a5a3b6b5a3cee0f7095db30a0
eugenishef <eugenishef@yandex.ru>
2024-10-17 14:54:58 +0300

    вернул аннотацию Data

--- stats-http-service/src/main/java/ru/practicum/stats/model/Statistics.java
+++ stats-http-service/src/main/java/ru/practicum/stats/model/Statistics.java
@@ -1,18 +1,14 @@
 package ru.practicum.stats.model;
 
 import jakarta.persistence.*;
-import lombok.*;
-import org.hibernate.proxy.HibernateProxy;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 @Entity
 @Table(name = "statistics")
-@Getter
-@Setter
-@ToString
-@RequiredArgsConstructor
+@Data
 @NoArgsConstructor
 public class Statistics {
     @Id
@@ -30,20 +26,4 @@ public class Statistics {
 
     @Column(name = "request_time", nullable = false)
     private LocalDateTime requestTime;
-
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null) return false;
-        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
-        if (thisEffectiveClass != oEffectiveClass) return false;
-        Statistics that = (Statistics) o;
-        return getId() != null && Objects.equals(getId(), that.getId());
-    }
-
-    @Override
-    public final int hashCode() {
-        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
-    }
 }
