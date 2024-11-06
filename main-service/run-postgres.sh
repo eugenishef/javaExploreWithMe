1a2d4aa4b43dc224a885bbacfbac440342714da9
eugenishef <eugenishef@yandex.ru>
2024-11-07 00:01:33 +0300

    небольшие правки настроек

--- main-service/run-postgres.sh
+++ main-service/run-postgres.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker stop db || true
-docker rm db || true
+docker stop main_db || true
+docker rm main_db || true
 
 docker run -d \
-  --name main-db \
+  --name main_db \
   -e POSTGRES_DB=main_db \
   -e POSTGRES_USER=postgres \
   -e POSTGRES_PASSWORD=postgres \
@@ -12,7 +12,7 @@ docker run -d \
   postgres:16.1
 
 if [ $? -eq 0 ]; then
-  echo "База данных 'db' успешно создана и запущена."
+  echo "База данных 'main_db' успешно создана и запущена."
 else
   echo "Не удалось создать или запустить базу данных."
 fi
